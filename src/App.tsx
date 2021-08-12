98510a724b9b7fa851cd9b4e1c27a39bf0fdcc89
Craig Belton II <csbeltonii@gmail.com>
2021-08-12 15:56:31 -0400

    changed routes

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/dealer/:accountNumber" element={<Dealer />} />
         <Route path="/search" element={<SearchResults />} />
       </Routes>
